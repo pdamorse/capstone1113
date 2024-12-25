449f06bd0b7ff64a1255951611b17158968d1653
pabloon4 <pdamorse@gmail.com>
2024-12-25 18:30:35 -0500

    Integrated Jinga2 framework into HTML and CSS

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates/')
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def homepage():
+    return render_template("index.html.j2", title="Hello")
+
+@app.route("/About")
+def about_us():
+    return render_template("about.html.j2", title="About Us")
+
+@app.route("/Login")
+def login_page():
+    return render_template("login.html.j2", title="Login")
